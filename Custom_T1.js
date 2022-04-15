840b53ff790a49cf5d072601658f0159f5cb0d34
enoch-exe-inc <47507476+enoch-exe-inc@users.noreply.github.com>
2022-04-15 15:09:53 -0400

    Update Custom_T1.js

--- Custom_T1.js
+++ Custom_T1.js
@@ -79,7 +79,7 @@ var init = () => {
 	{
 		let getDesc = (level) => "c_4=10^{" + level + "}";
 		let getInfo = (level) => "c_4=" + getC4(level).toString(0);
-		c4 = theory.createUpgrade(5, currency, new ExponentialCost(1e9, 9 * Math.log2(10)));	// Original: (1e10, 8 * Math.log2(10))
+		c4 = theory.createUpgrade(5, currency, new ExponentialCost(1e8, 8 * Math.log2(10)));	// Original: (1e10, 8 * Math.log2(10))
 		c4.getDescription = (_) => Utils.getMath(getDesc(c4.level));
 		c4.getInfo = (amount) => Utils.getMathTo(getInfo(c4.level), getInfo(c4.level + amount));
 		c4.isAvailable = false;
@@ -90,7 +90,7 @@ var init = () => {
 	{
 		let getDesc = (level) => "c_5=10^{" + level + "}";
 		let getInfo = (level) => "c_5=" + getC5(level).toString(0);
-		c5 = theory.createUpgrade(6, currency, new ExponentialCost(1e30, 12 * Math.log2(10)));	// TODO: change to 1e12
+		c5 = theory.createUpgrade(6, currency, new ExponentialCost(1e12, 12 * Math.log2(10)));	// TODO: change to 1e12
 		c5.getDescription = (_) => Utils.getMath(getDesc(c5.level));
 		c5.getInfo = (amount) => Utils.getMathTo(getInfo(c5.level), getInfo(c5.level + amount));
 		c5.isAvailable = false;
