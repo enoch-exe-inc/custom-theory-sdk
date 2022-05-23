bdaf834d4d6aade2001809c2d1a747eb4472289b
enoch-exe-inc <47507476+enoch-exe-inc@users.noreply.github.com>
2022-05-23 11:34:29 -0400

    Update Custom_T2.js

--- Custom_T2.js
+++ Custom_T2.js
@@ -104,7 +104,7 @@ var init = () => {
 
 	//////////////////////
 	// Checkpoint Upgrades
-	theory.setMilestoneCost(new LinearCost(25, 25));
+	theory.setMilestoneCost(new LinearCost(2.5, 2.5));	// Original: (25, 25) - Gain 1 milestone upgrade per 1e25 of tau
 
 	{
 		qTerms = theory.createMilestoneUpgrade(0, 2);
