b07e01c27cf7628deefa18df6ffa7780f5bb8f40
enoch-exe-inc <47507476+enoch-exe-inc@users.noreply.github.com>
2023-01-02 14:53:49 -0500

    Added a c3 variable

--- CustomTheory.js
+++ CustomTheory.js
@@ -11,11 +11,11 @@ var authors = "Gilles-Philippe Paillé";
 var version = 1;
 
 var currency;
-var c1, c2;
+var c1, c2, c3;
 var c1Exp, c2Exp;
 
-var achievement1, achievement2;
-var chapter1, chapter2;
+// var achievement1, achievement2;
+// var chapter1, chapter2;
 
 var init = () => {
     currency = theory.createCurrency();
@@ -35,30 +35,39 @@ var init = () => {
     {
         let getDesc = (level) => "c_2=2^{" + level + "}";
         let getInfo = (level) => "c_2=" + getC2(level).toString(0);
-        c2 = theory.createUpgrade(1, currency, new ExponentialCost(1, Math.log2(4)));
+        c2 = theory.createUpgrade(1, currency, new ExponentialCost(2, Math.log2(4)));
         c2.getDescription = (_) => Utils.getMath(getDesc(c2.level));
         c2.getInfo = (amount) => Utils.getMathTo(getInfo(c2.level), getInfo(c2.level + amount));
+    }
+	
+	// c3
+    {
+        let getDesc = (level) => "c_3=3^{" + level + "}";
+        let getInfo = (level) => "c_3=" + getC3(level).toString(0);
+        c3 = theory.createUpgrade(1, currency, new ExponentialCost(1, Math.log2(8)));
+        c3.getDescription = (_) => Utils.getMath(getDesc(c3.level));
+        c3.getInfo = (amount) => Utils.getMathTo(getInfo(c3.level), getInfo(c3.level + amount));
     }
 
     /////////////////////
     // Permanent Upgrades
-    theory.createPublicationUpgrade(0, currency, 1e10);
-    theory.createBuyAllUpgrade(1, currency, 1e13);
-    theory.createAutoBuyerUpgrade(2, currency, 1e30);
+    theory.createPublicationUpgrade(0, currency, 1e9);
+    theory.createBuyAllUpgrade(1, currency, 1e12);
+    theory.createAutoBuyerUpgrade(2, currency, 1e25);
 
     ///////////////////////
     //// Milestone Upgrades
     theory.setMilestoneCost(new LinearCost(25, 25));
 
     {
-        c1Exp = theory.createMilestoneUpgrade(0, 3);
+        c1Exp = theory.createMilestoneUpgrade(1, 3);
         c1Exp.description = Localization.getUpgradeIncCustomExpDesc("c_1", "0.05");
         c1Exp.info = Localization.getUpgradeIncCustomExpInfo("c_1", "0.05");
         c1Exp.boughtOrRefunded = (_) => theory.invalidatePrimaryEquation();
     }
 
     {
-        c2Exp = theory.createMilestoneUpgrade(1, 3);
+        c2Exp = theory.createMilestoneUpgrade(2, 3);
         c2Exp.description = Localization.getUpgradeIncCustomExpDesc("c_2", "0.05");
         c2Exp.info = Localization.getUpgradeIncCustomExpInfo("c_2", "0.05");
         c2Exp.boughtOrRefunded = (_) => theory.invalidatePrimaryEquation();
@@ -66,13 +75,13 @@ var init = () => {
     
     /////////////////
     //// Achievements
-    achievement1 = theory.createAchievement(0, "Achievement 1", "Description 1", () => c1.level > 1);
-    achievement2 = theory.createSecretAchievement(1, "Achievement 2", "Description 2", "Maybe you should buy two levels of c2?", () => c2.level > 1);
+    // achievement1 = theory.createAchievement(0, "Achievement 1", "Description 1", () => c1.level > 1);
+    // achievement2 = theory.createSecretAchievement(1, "Achievement 2", "Description 2", "Maybe you should buy two levels of c2?", () => c2.level > 1);
 
     ///////////////////
     //// Story chapters
-    chapter1 = theory.createStoryChapter(0, "My First Chapter", "This is line 1,\nand this is line 2.\n\nNice.", () => c1.level > 0);
-    chapter2 = theory.createStoryChapter(1, "My Second Chapter", "This is line 1 again,\nand this is line 2... again.\n\nNice again.", () => c2.level > 0);
+    // chapter1 = theory.createStoryChapter(0, "My First Chapter", "This is line 1,\nand this is line 2.\n\nNice.", () => c1.level > 0);
+    // chapter2 = theory.createStoryChapter(1, "My Second Chapter", "This is line 1 again,\nand this is line 2... again.\n\nNice again.", () => c2.level > 0);
 
     updateAvailability();
 }
@@ -84,8 +93,7 @@ var updateAvailability = () => {
 var tick = (elapsedTime, multiplier) => {
     let dt = BigNumber.from(elapsedTime * multiplier);
     let bonus = theory.publicationMultiplier;
-    currency.value += dt * bonus * getC1(c1.level).pow(getC1Exponent(c1Exp.level)) *
-                                   getC2(c2.level).pow(getC2Exponent(c2Exp.level));
+    currency.value += dt * bonus * getC1(c1.level).pow(getC1Exponent(c1Exp.level)) * getC2(c2.level).pow(getC2Exponent(c2Exp.level)) * getC3(c3.level);
 }
 
 var getPrimaryEquation = () => {
@@ -100,6 +108,8 @@ var getPrimaryEquation = () => {
     if (c2Exp.level == 1) result += "^{1.05}";
     if (c2Exp.level == 2) result += "^{1.1}";
     if (c2Exp.level == 3) result += "^{1.15}";
+	
+	result += "c_3";
 
     return result;
 }
@@ -112,6 +122,7 @@ var get2DGraphValue = () => currency.value.sign * (BigNumber.ONE + currency.valu
 
 var getC1 = (level) => Utils.getStepwisePowerSum(level, 2, 10, 0);
 var getC2 = (level) => BigNumber.TWO.pow(level);
+var getC3 = (level) => BigNumber.TWO.pow(level);
 var getC1Exponent = (level) => BigNumber.from(1 + 0.05 * level);
 var getC2Exponent = (level) => BigNumber.from(1 + 0.05 * level);
 
